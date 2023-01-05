9a94514bae2f7c5154e4dcbc7104227cb3b3eff5
Ivan Gamboa Ultreras <gamboita9@hotmail.com>
2023-01-05 13:26:34 -0600

    SOAP API: Add Testing
    Add Config tests

---
+++ soapapi/src/test/java/choice/university/ivan/soapapi/config/AppConfigTest.java
@@ -0,0 +1,30 @@
+package choice.university.ivan.soapapi.config;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.XsdSchema;
+
+@SpringBootTest
+@ContextConfiguration(classes = AppConfig.class)
+public class AppConfigTest {
+    @Autowired
+    private ServletRegistrationBean<MessageDispatcherServlet> servletRegistrationBean;
+    @Autowired
+    private DefaultWsdl11Definition defaultWsdl11Definition;
+    @Autowired
+    private XsdSchema xsdSchema;
+
+    @Test
+    void testContextLoads() {
+        assertNotNull(servletRegistrationBean);
+        assertNotNull(defaultWsdl11Definition);
+        assertNotNull(xsdSchema);
+    }
+}
